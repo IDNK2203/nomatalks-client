da01abcaad0bbe80005fe4382650a467a9777a9d
idnk <efeidukpaye@gmail.com>
2020-11-30 22:57:11 +0100

    init home page and blog details page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,19 @@
-import "@glidejs/glide";
-// import "popper.js";
-// import "jquery";
+
 import "bootstrap";
+
+import Glide from '@glidejs/glide';
+
+var glide = new Glide('.glide', {
+    type: 'carousel',
+    startAt:0,
+    perView: 1,
+    focusAt: 0,
+    // autoplay:3000,
+    // hoverpause: true,
+    animationTimingFunc: "ease-in-out",
+    animationDuration: 800,
+  })
+  
+
+glide.mount()
+
